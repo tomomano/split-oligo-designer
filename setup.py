53f51a5709bda970795ea552226d6c6efccc2472
Tomoyuki Mano <tomoyukimano@gmail.com>
2022-12-05 18:04:02 +0900

    add setup.py; minor revision in jupyter notebook

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name="oligodesigner",
+	version="0.0.1",
+	author="",
+	author_email="",
+	description="This repository contains the code to design the oligonucleotides for mFISH3D.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	]
+)
