4929eea069afce2f58f70799ef67e29d2950972e
tmurakami <cunshang.m.tatsuya@gmail.com>
2024-06-19 08:28:00 -0400

    add v4 HCR design

--- oligodesigner/parameters.py
+++ oligodesigner/parameters.py
@@ -59,30 +59,25 @@ hcr_two_sides = {
 
 
 
-# hcr_b_set = {
-#     'B1':{
-#         'splitseq_ll':'GAGGAGGGCAGCAAACGG','splitseq_rr':'GAAGAGTCTTCCTTTACG','anchorseq_ll':'aa','anchorseq_rr':'at',
-#         'splitseq_lr':'GGCAGCAAACGGGAAGAG','splitseq_rl':'GCATTCTTTCTTGAGGAG','anchorseq_lr':'tt','anchorseq_rl':'tt'
-#     },
-#     'B2':{
-#         'splitseq_ll':'CCTCGTAAATCCTCATCA','splitseq_rr':'ATCATCCAGTAAACCGCC','anchorseq_ll':'aa','anchorseq_rr':'aa',
-#         'splitseq_lr':'AAATCCTCATCAATCATC','splitseq_rl':'AGCTCAGTCCATCCTCGT','anchorseq_lr':'ta','anchorseq_rl':'ta'
-#     },
-#     'B3':{
-#         'splitseq_ll':'GTCCCTGCCTCTATATCT','splitseq_rr':'CCACTCAACTTTAACCCG','anchorseq_ll':'tt','anchorseq_rr':'tt',
-#         'splitseq_lr':'GCCTCTATATCTCCACTC','splitseq_rl':'AAAGTCTAATCCGTCCCT','anchorseq_lr':'ta','anchorseq_rl':'aa'
-#     },
-#     'B4':{
-#         'splitseq_ll':'CCTCAACCTACCTCCAAC','splitseq_rr':'TCTCACCATATTCGCTTC','anchorseq_ll':'aa','anchorseq_rr':'at',
-#         'splitseq_lr':'CCTACCTCCAACTCTCAC','splitseq_rl':'CACATTTACAGACCTCAA','anchorseq_lr':'tt','anchorseq_rl':'at'
-#     },
-#     'B5':{
-#         'splitseq_ll':'CTCACTCCCAATCTCTAT','splitseq_rr':'CTACCCTACAAATCCAAT','anchorseq_ll':'aa','anchorseq_rr':'aa',
-#         'splitseq_lr':'CCCAATCTCTATCTACCC','splitseq_rl':'CACTTCATATCACTCACT','anchorseq_lr':'at','anchorseq_rl':'aa'
-#     },
-#     'B8':{
-#         'splitseq_ll':'CCTTGATTATCTCGTCTC','splitseq_rr':'CATCACTCGCACTCTACC','anchorseq_ll':'at','anchorseq_rr':'aa',
-#         'splitseq_lr':'TCTACCGTCAAGTCAAAC','splitseq_rl':'CGTCTCCATCACTCGCAC','anchorseq_lr':'aa','anchorseq_rl':'aa'
-#     }
-# }
-
+hcr_one_side_v4 = {
+    'B1':{
+        'seq_even_l':'GAGGAGGGCAGCAAACGGGAta','seq_odd_r':'atAGAGTCTTCCTTTACG',
+        'seq_even_r':'','seq_odd_l':''
+    },
+    'B2':{
+        'seq_even_l':'CCTCGTAAATCCTCATCAATat','seq_odd_r':'taCATCCAGTAAACCGCC',
+        'seq_even_r':'','seq_odd_l':''
+    },
+    'B3':{
+        'seq_even_l':'GTCCCTGCCTCTATATCTCCta','seq_odd_r':'aaACTCAACTTTAACCCG',
+        'seq_even_r':'','seq_odd_l':''
+    },
+    'B4':{
+        'seq_even_l':'CCTCAACCTACCTCCAACTCaa','seq_odd_r':'aaTCACCATATTCGCTTC',
+        'seq_even_r':'','seq_odd_l':''
+    },
+    'B5':{
+        'seq_even_l':'CTCACTCCCAATCTCTATCTta','seq_odd_r':'aaACCCTACAAATCCAAT',
+        'seq_even_r':'','seq_odd_l':''
+    }
+}
